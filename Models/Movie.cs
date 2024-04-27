34edee35902d9921d0367fb14ee6c8e7fcbf9550
Colin Elliott <79231253+Hexagenia@users.noreply.github.com>
2024-04-27 19:03:26 -0400

    Add files via upload

---
+++ Models/Movie.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(60,MinimumLength =3)]
+        [Required]
+        [Display(Name = "Make")]
+        public string? Title { get; set; } = string.Empty;
+
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date (Year)")]
+        public DateTime? ReleaseDate { get; set; }
+
+
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Model")]
+        public string? Genre { get; set; } = string.Empty;
+
+
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [Display(Name = "Price new (not what I paid)")]
+        public decimal Price { get; set; } = 0;
+
+
+        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        
+        public string Rating { get; set; } = string.Empty;
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+
+namespace RazorPagesMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if (context == null || context.Movie == null) 
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                //looks for movies
+                if (context.Movie.Any()) 
+                {
+                    return; 
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Toyota",
+                        ReleaseDate = DateTime.Parse("2007-1-1"),
+                        Genre = "Tacoma",
+                        Price = 7.99M,
+                        Rating = "TRDOR"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Nissan",
+                        ReleaseDate = DateTime.Parse("2013-1-1"),
+                        Genre = "370Z",
+                        Price = 8.99M,
+                        Rating = "Sport"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Honda",
+                        ReleaseDate = DateTime.Parse("2003-1-1"),
+                        Genre = "Civic",
+                        Price = 9.99M,
+                        Rating = "DX"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Volvo",
+                        ReleaseDate = DateTime.Parse("2007-1-1"),
+                        Genre = "S60",
+                        Price = 3.99M,
+                        Rating = "R"
+                    }
+
+
+
+                    );
+                    context.SaveChanges();
+            }
+                
+        }
+
+
+
+
+
+
+
+
+    }
+}
